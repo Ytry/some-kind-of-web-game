3121751514f19fbad630a0d474a385aaf323a404
Ytry <Kyle12qw@gmail.com>
2018-02-22 17:56:53 -0500

    removed unused imports
    Removed some usused imports

--- backend.py
+++ backend.py
@@ -1,15 +1,4 @@
 from flask import Flask, render_template, url_for, redirect, request, flash
-from flask import session as login_session
-from flask import make_response, jsonify
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-import random
-import string
-import httplib2
-import json
-import requests
 
 app = Flask(__name__)
 
